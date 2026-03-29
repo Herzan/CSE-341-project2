250bfda71ede782507986183c6f38605553e9421
Herzan <46695573+Herzan@users.noreply.github.com>
2026-03-28 20:20:12 -0600

    f

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,9 @@ const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.status(401).json({ message: 'Unauthorized - Please log in with Google' });
+  res.status(401).json({ 
+    message: 'Unauthorized - Please log in with GitHub first' 
+  });
 };
 
 module.exports = { isAuthenticated };
