e66c3c5bb606fd85ebc9a6130c116565d27e3a49
Herzan <46695573+Herzan@users.noreply.github.com>
2026-03-28 18:44:00 -0600

    hhh

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,16 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const callbackURL = isProduction 
+  ? 'https://cse-341-project2-ea66.onrender.com/api/auth/google/callback'
+  : 'http://localhost:5000/api/auth/google/callback';
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'   // will be relative to your Render URL
+    callbackURL: callbackURL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
@@ -16,11 +22,12 @@ passport.use(new GoogleStrategy({
           googleId: profile.id,
           displayName: profile.displayName,
           email: profile.emails[0].value,
-          photo: profile.photos[0].value
+          photo: profile.photos?.[0]?.value || null
         }).save();
       }
       return done(null, user);
     } catch (err) {
+      console.error('Google Strategy Error:', err);
       return done(err, null);
     }
   }
@@ -28,8 +35,12 @@ passport.use(new GoogleStrategy({
 
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
-  done(null, user);
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 module.exports = passport;
